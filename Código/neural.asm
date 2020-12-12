18b72f84d31d1d9518b5b134e559736139fbcc9f
Vinícius Marini <57442852+marinisz@users.noreply.github.com>
2020-12-12 11:20:13 -0300

    mark1

---
+++ Código/neural.asm
@@ -0,0 +1,13 @@
+.data
+	
+.text
+    #adicionar o valor nas variaveis
+
+    #multiplicando e armazenando
+
+    #calcular os pesos
+    
+    #ver se o resultado ta proximo
+    
+    #testar com a proxima linha
+    syscall
