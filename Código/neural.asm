c8f32424f7373e075d43aabfd6e604621cb62a8d
Vinícius Marini <57442852+marinisz@users.noreply.github.com>
2020-12-12 19:50:41 -0300

    Update neural.asm

--- Código/neural.asm
+++ Código/neural.asm
@@ -8,15 +8,29 @@
 	myFloat8: .float 8.00
 	myFloat10: .float 10.00
 	peso1: .float 0.00
-	peso2: .float 0.00
+	peso2: .float 0.80
 	zeroAsFloat: .float 0.00
 	taxaAprendizado: .float 0.05
+	erro1: .float 1.20
 .text
-	#salvar tx ded aprendizado
-	#gerar peso1 e peso2
-	#salvar epoca
-	#testar os dados
+	#load tx de aprendizado = 0.05
+	lwc1 $f0,taxaAprendizado
+	#salvar peso1 e peso2
+	lwc1 $f1,peso1
+	lwc1 $f2,peso2
+	#saida esperada na primeira linha
+	lwc1 $f3,myFloat2
+	lwc1 $f8,myFloat6
+	lwc1 $f9,myFloat1
+	#testar os dados{
+	
+    for:slt , , 	#5 pq vai passar pelas 5 linhas
+	beq , ,   #se for <6 para
+	mul.s $f4,$f1,$f5       #multiplica peso1 x i
+	mul.s $f4,$f2,$f5	#multiplica peso2 x i
+	
+ fimFor:	
 	#linha 0 - 1 x peso1+1 x peso2 = X     (x minusco = multi / X = variavel)
 	#2 (resutlado de 1+1)-X=erro
-	#atualizar o peso - pesox = pesox+erro x taxaAprendizado x 1(entrada linha0)
+	#atualizar o peso - pesox = pesox+erro x taxaAprendizado x 1(entrada linha0)}
 	#repetir tudo
